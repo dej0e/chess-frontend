dd466f5e7c1d5e75d48bc2e1461ce2f66f85f810
dej0e <dejoejohn@gmail.com>
2024-12-09 19:47:08 -0500

    Fixes title

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ChessGame from './components/ChessGame';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Chess - Web Sockets';
+  }, []);
+
   return (
     <div>
       <ChessGame />
